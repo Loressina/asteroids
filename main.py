9b5ee63f81228548576a7699f64241c9fc80bd3a
Loressina <loressina@posteo.de>
2025-04-15 21:23:42 +0200

    add shooting

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from constants import *
 from player import Player
+from shot import Shot
 
 def main():
     print("Starting Asteroids!")
@@ -18,35 +19,36 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
-
     asteroidField = AsteroidField()
+
+    Shot.containers = (updatable, drawable, shots)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        update_screen(screen, updatable, drawable, asteroids, player, dt)
+        
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.check_collision(player):
+                print("Game over!")
+                sys.exit(0)
+
+        screen.fill("black")
+        for d in drawable:
+            d.draw(screen)
+
+        pygame.display.flip()
         dt = clock.tick(60) / 1000
 
-def update_screen(screen, updatable, drawable, asteroids, player, dt):
-    updatable.update(dt)
-    for asteroid in asteroids:
-        if asteroid.check_collision(player):
-            print("Game over!")
-            sys.exit(0)
-
-    screen.fill("black")
-
-    for d in drawable:
-        d.draw(screen)
-
-    pygame.display.flip()
 
 if __name__ == "__main__":
     main()
