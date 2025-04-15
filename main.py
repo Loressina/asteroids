ad53fbe1d0ccb31eeb90b77d7aa8ea5a92cf49a4
Loressina <loressina@posteo.de>
2025-04-15 07:23:23 +0200

    add main game loop

--- main.py
+++ main.py
@@ -6,5 +6,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        update_screen(screen)
+
+def update_screen(screen):
+    screen.fill("black")
+    pygame.display.flip()
+
 if __name__ == "__main__":
     main()
